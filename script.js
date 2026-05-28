922950e10683d38b38d22b1dccb1ef59047efa0a
Vupi <276366791+FMD911@users.noreply.github.com>
2026-05-28 12:08:23 +0200

    add win and draw logic to game controller

--- script.js
+++ script.js
@@ -29,6 +29,18 @@ const GameController = (() => {
   const playerO = Player("Player 2", "O");
 
   let currentPlayer = playerX;
+  let gameOver = false;
+
+  const winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
 
   function switchPlayer() {
     currentPlayer = currentPlayer === playerX ? playerO : playerX;
@@ -38,8 +50,35 @@ const GameController = (() => {
     return currentPlayer;
   }
 
+  function checkWin(board) {
+    return winCombos.some(combo => {
+      return combo.every(index => board[index] === currentPlayer.marker);
+    });
+  }
+
+  function checkDraw(board) {
+    return board.every(cell => cell !== "");
+  }
+
   function playTurn(index) {
+    if (gameOver) return;
+
     Gameboard.placeMark(index, currentPlayer.marker);
+
+    const board = Gameboard.getBoard();
+
+    if (checkWin(board)) {
+      gameOver = true;
+      console.log(`${currentPlayer.name} wins!`);
+      return;
+    }
+
+    if (checkDraw(board)) {
+      gameOver = true;
+      console.log("It's a draw!");
+      return;
+    }
+
     switchPlayer();
   }
 
